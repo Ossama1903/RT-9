856f60e8eeee89569d1cfbda589ad133b6e8f965
Ossama1903 <sheikhusamabilal@gmail.com>
2023-04-19 17:26:42 +0500

    more code

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const user = require("./routes/user");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
 const app = express();
 const port = 4000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const MONGO_URI = `mongodb+srv://kage1903:mongo19@rt-9-cluster.wx4nhre.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
+
 app.use("/user", user);
 
 app.listen(port, () => {
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,10 +1,124 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { signup } from "../../API/user";
 
 function Signup() {
+  const [file, setFile] = useState(null);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [gender, setGender] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+
+  const inputs = [
+    {
+      name: "firstName",
+      placeHolder: "First Name",
+      type: "text",
+      onChange: (e) => {
+        setFirstName(e.target.value);
+      },
+      isValid: firstName !== "",
+    },
+    {
+      name: "lastName",
+      placeHolder: "Last Name",
+      type: "text",
+      onChange: (e) => {
+        setLastName(e.target.value);
+      },
+      isValid: lastName !== "",
+    },
+    {
+      name: "email",
+      placeHolder: "Email",
+      type: "text",
+      onChange: (e) => {
+        setEmail(e.target.value);
+      },
+      isValid: () => {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(email);
+      },
+    },
+    {
+      name: "password",
+      placeHolder: "Password",
+      type: "password",
+      onChange: (e) => {
+        setPassword(e.target.value);
+      },
+      isValid: () => {
+        const re =
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        return re.test(password);
+      },
+    },
+    {
+      name: "dateOfBirth",
+      placeHolder: "Date of Birth",
+      type: "date",
+      onChange: (e) => {
+        setDateOfBirth(e.target.value);
+      },
+      isValid: dateOfBirth !== "",
+    },
+    {
+      name: "phoneNumber",
+      placeHolder: "Contact",
+      type: "text",
+      onChange: (e) => {
+        setPhoneNumber(e.target.value);
+      },
+      isValid: phoneNumber !== "",
+    },
+    {
+      name: "address",
+      placeHolder: "Address",
+      type: "text",
+      onChange: (e) => {
+        setAddress(e.target.value);
+      },
+      isValid: address !== "",
+    },
+    {
+      name: "profilePicture",
+      placeHolder: "Profile Picture",
+      type: "filetext",
+      onChange: (e) => {
+        setFile(e.target.value);
+      },
+      isValid: Boolean(file),
+    },
+  ];
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       <h1>Signup Page</h1>
       <p>Sign up for my app!</p>
+      <form onSubmit={handleSubmit}>
+        {inputs.map((input) => (
+          <input
+            type={input.type}
+            placeholder={input.placeHolder}
+            onChange={input.onChange}
+          ></input>
+        ))}
+        <input type="file" onChange={handleFileChange} />
+        <button type="submit" onClick={handleSubmit}>
+          Upload
+        </button>
+      </form>
     </div>
   );
 }
